6cdbead60bb87aa23042b969b7d04b7cbce10e31
t4robertmitroi <120459991+t4robertmitroi@users.noreply.github.com>
2024-03-11 23:41:28 +0200

    change config for neotree and telescope and added gitsigns line_blame

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,19 +1,31 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          ".github",
-          ".gitignore",
-          "node_modules/",
-          -- "package-lock.json",
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".github",
+            ".gitignore",
+            "node_modules/",
+            -- "package-lock.json",
+          },
+          never_show = { ".git", "node_modules" },
         },
-        never_show = { ".git", "node_modules" },
+        bind_to_cwd = true,
+        follow_current_file = {
+          enabled = false, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+        use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
       },
-    },
-  },
+      buffers = {
+        bind_to_cwd = true,
+      },
+    })
+  end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,11 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.5",
   config = function()
+    require("telescope").setup({
+      defaults = {
+        path_display = { "truncate" },
+      },
+    })
     vim.keymap.set(
       "n",
       "<leader>ff",
