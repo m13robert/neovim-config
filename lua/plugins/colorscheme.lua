eaa6cd22d744f8680c8df3a573d750d33c0acf69
t4robertmitroi <120459991+t4robertmitroi@users.noreply.github.com>
2024-03-19 16:56:01 +0200

    added chatgpt and enable neo-tree follow file

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,17 @@
 return {
-  "polirritmico/monokai-nightasty.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("monokai-nightasty")
-  end,
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+  -- "polirritmico/monokai-nightasty.nvim",
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --   vim.cmd.colorscheme("monokai-nightasty")
+  -- end,
 }
