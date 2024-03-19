7a5aec63ecb578e852bf14af451277ddbcfa910f
t4robertmitroi <120459991+t4robertmitroi@users.noreply.github.com>
2024-03-19 17:15:55 +0200

    added gitui and chatgpt plugin

--- init.lua
+++ init.lua
@@ -1,15 +1,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim",                           import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.util.gitui" },
+    { import = "plugins" },
+  },
+})
