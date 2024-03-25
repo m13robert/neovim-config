b1e93de56915b9d2d46ec1437b8323153bf4643b
t4robertmitroi <120459991+t4robertmitroi@users.noreply.github.com>
2024-03-25 13:22:43 +0200

    added live grep from root

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,10 +7,13 @@ return {
         path_display = { "truncate" },
       },
     })
+    local builtin = require("telescope.builtin")
+    local utils = require("telescope.utils")
     vim.keymap.set(
       "n",
       "<leader>ff",
       "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>"
     )
+    vim.keymap.set("n", "<leader>/", builtin.live_grep, { cwd = utils.buffer_dir() })
   end,
 }
