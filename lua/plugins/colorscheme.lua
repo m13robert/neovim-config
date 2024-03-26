2bbe99ffe46e26e8cee30551f758297ef7234a6e
t4robertmitroi <120459991+t4robertmitroi@users.noreply.github.com>
2024-03-26 17:08:13 +0200

    swapped colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,19 @@
 return {
   {
-    "catppuccin/nvim",
+    -- "catppuccin/nvim",
+    -- lazy = false,
+    -- name = "catppuccin",
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme("catppuccin-mocha")
+    -- end,
+  },
+  {
+    "polirritmico/monokai-nightasty.nvim",
     lazy = false,
-    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
+      vim.cmd.colorscheme("monokai-nightasty")
     end,
   },
-  -- "polirritmico/monokai-nightasty.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- config = function()
-  --   vim.cmd.colorscheme("monokai-nightasty")
-  -- end,
 }
