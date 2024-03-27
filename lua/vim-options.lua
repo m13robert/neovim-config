987ba6d63539a5a1c730800f58cc1a1d5f72631d
t4robertmitroi <120459991+t4robertmitroi@users.noreply.github.com>
2024-03-27 11:46:00 +0200

    changed status column to reflect changes in files

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,7 +5,8 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard+=unnamedplus")
 vim.g.mapleader = " "
 
-vim.opt.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum} "
+vim.opt.numberwidth = 3
+vim.opt.statuscolumn = "%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum . '  ' : v:lnum) : ''}%=%s"
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
