c59b9446cd4776d18f50412d68a3b6e5a485bd61
Robert Mitroi <robert13mitroi@gmail.com>
2024-09-20 14:32:44 +0300

    show hidden files in oil by default

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,12 @@ return {
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
+    })
     vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "File explorer" })
   end,
 }
